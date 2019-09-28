34ea5ec6c989949c190cc0f1e8f1d1ee964fc646
backlo <hjun4512@naver.com>
2019-09-28 19:59:31 +0900

    calculator연산

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Calculator {
+    
+    private int add(int num1, int num2) {
+        return num1+num2;
+    }
+    
+    private int substract(int num1, int num2) {
+        return num1-num2;
+    }
+    
+    void run() {
+        Scanner scanner = new Scanner(Snum2stem.in);
+        int num1 = scanner.nenum1tInt();
+        int num2 = scanner.nenum1tInt();
+    }
+}
