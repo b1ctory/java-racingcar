4a873285b2821cea02dc8245d17cf35b57327ba2
b1ctory <suebin710@gmail.com>
2019-10-10 20:05:15 +0900

    [자동차 게임] 피드백 반영하여 수정하였습니다.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # java-racingcar
 * [코드 리뷰1](https://github.com/woowacourse/woowacourse-docs/blob/master/maincourse/review-step1.md)
 * [코드 리뷰2](https://github.com/woowacourse/woowacourse-docs/blob/master/maincourse/review-step2.md)
+
+##* java-racingcar 기능 설명
+   1) Client에서 사용자에게 Car Name과 Playtime을 입력받고 CarManager에게 전달한다.
+   2) Car 객체는 자동차의 전진 유무를 결정하고, 자신의 위치를 출력한다.
+   3) CarManager 객체는 Car 객체를 생성하고, Racing을 진행하고, 우승자를 가려낸다.
+   
--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,7 +1,9 @@
+import java.util.Random;
+
 public class Car{
     private final String name;
     private int position = 0;
-
+    private Random random = new Random();
     public Car(String name){
         this.name = name;
     }
@@ -14,7 +16,19 @@ public class Car{
         return this.name;
     }
 
-    public void sumPosition(int position) {
-        this.position += position;
+    public void movePosition() {
+        this.position += movingCar(random.nextInt(10));
+    }
+
+    public int movingCar(int random) {
+        if (random > 3) return 1;
+        return 0;
+    }
+
+    public void printMove(){
+        for(int i = 0; i < position; i++){
+            System.out.print("-");
+        }
+        System.out.println();
     }
 }
