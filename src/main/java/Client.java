dbfff8740aa4d6597e98e95210be4ed81ee543a9
b1ctory <suebin710@gmail.com>
2019-10-15 13:31:40 +0900

    RacingCar - Client에서 게임이 실행됩니다.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,19 +1,7 @@
 import java.util.Scanner;
 
 public class Client {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)기준으로 구분");
-        String carName = scanner.nextLine();
-
-        System.out.println("시도할 횟수는 몇 회 인가요?");
-        int playTime = scanner.nextInt();
-
-        CarManager cm = new CarManager(carName, playTime);
-
-        cm.putCarName();
-        cm.playResult(playTime);
-        cm.result();
+        new CarRacingView();
     }
 }
--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-import calculator.Calculator;
-
-public class Main {
-    public static void main(String[] args) {
-        new Calculator().run();
-    }
-}
